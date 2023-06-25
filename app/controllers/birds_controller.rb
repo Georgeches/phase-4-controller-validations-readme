1eeb0f807a6995d8a9709ebad4c7a428c4ac5fcd
Georgeches <georgechesire83@gmail.com>
2023-06-25 09:31:37 +0300

    Done assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,10 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  def render_unprocessable_entity_response(invalid)
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  end
 
   # GET /birds
   def index
@@ -9,7 +14,7 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
   end
 
@@ -22,7 +27,7 @@ class BirdsController < ApplicationController
   # PATCH /birds/:id
   def update
     bird = find_bird
-    bird.update(bird_params)
+    bird.update!(bird_params)
     render json: bird
   end
 
